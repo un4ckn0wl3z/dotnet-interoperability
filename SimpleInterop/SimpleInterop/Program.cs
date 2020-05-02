43cb1278ae035eac90b27ebeab860cae8aa7d958
un4ckn0wl3z <slickerth@gmail.com>
2020-05-02 21:55:26 +0700

    init

---
+++ SimpleInterop/SimpleInterop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInterop
+{
+    class Program
+    {
+        [DllImport("user32")]
+        extern static bool MessageBeep(uint sound);
+
+        static void Main(string[] args)
+        {
+            MessageBeep(0x10);
+        }
+    }
+}
