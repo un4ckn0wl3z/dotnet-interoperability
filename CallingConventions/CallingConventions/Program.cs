d24fdbd1fc2fe56b2dc0d3ebb082a65688fd2d93
un4ckn0wl3z <slickerth@gmail.com>
2020-05-03 08:36:41 +0700

    add calling convention

---
+++ CallingConventions/CallingConventions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingConventions
+{
+    class Program
+    {
+        [DllImport("msvcrt", CallingConvention = CallingConvention.Cdecl)]
+        extern static int sprintf(StringBuilder buffer, string format, int id);
+
+        static void Main(string[] args)
+        {
+            var buffer = new StringBuilder(128);
+            sprintf(buffer, "Process %d is using sprintf!", Process.GetCurrentProcess().Id);
+            Console.WriteLine(buffer.ToString());
+            Console.ReadKey();
+        }
+    }
+}
