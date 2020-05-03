1b9cad7c496745d3dde514080a2e48d1a1f681f1
un4ckn0wl3z <slickerth@gmail.com>
2020-05-03 09:58:14 +0700

    add structure and union

---
+++ StructuresAndUnions/StructuresAndUnions/NativeFunctions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuresAndUnions
+{
+
+    public enum VirtualKeys : ushort
+    {
+        VK_0 = 0x30,
+        VK_PLUS = 0x6b,
+        VK_ENTER = 0xd
+    }
+
+    public enum InputType : uint
+    {
+        INPUT_MOUSE = 0,
+        INPUT_KEYBOARD = 1
+
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct MOUSEINPUT
+    {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct KEYBDINPUT
+    {
+        public VirtualKeys wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+
+
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct INPUT
+    {
+        [FieldOffset(0)]
+        public InputType type;
+        [FieldOffset(4)]
+        public MOUSEINPUT mi;
+        [FieldOffset(4)]
+        public KEYBDINPUT ki;
+        //FieldOffset(4)]
+        //HARDWAREINPUT hi;
+
+
+    }
+
+
+    class NativeFunctions
+    {
+        [DllImport("user32")]
+        public static extern bool SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32")]
+        public static extern uint SendInput(uint count,
+            [MarshalAs(UnmanagedType.LPArray)] INPUT[] inputs,
+            int size);
+    }
+}
---
+++ StructuresAndUnions/StructuresAndUnions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StructuresAndUnions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = Process.GetProcessesByName("calc1")[0];
+            var hCalc = calc.MainWindowHandle;
+            NativeFunctions.SetForegroundWindow(hCalc);
+
+            INPUT[] inputs = new INPUT[1];
+            //Thread.Sleep(2000);
+            inputs[0].type = InputType.INPUT_KEYBOARD;
+            inputs[0].ki.wVk = VirtualKeys.VK_0 + 3;
+            NativeFunctions.SendInput(1, inputs, Marshal.SizeOf(inputs[0]));
+            //Thread.Sleep(2000);
+
+            inputs[0].ki.wVk = VirtualKeys.VK_PLUS;
+            NativeFunctions.SendInput(1, inputs, Marshal.SizeOf(inputs[0]));
+            //Thread.Sleep(2000);
+
+            inputs[0].ki.wVk = VirtualKeys.VK_0 + 4;
+            NativeFunctions.SendInput(1, inputs, Marshal.SizeOf(inputs[0]));
+            //Thread.Sleep(2000);
+
+            inputs[0].ki.wVk = VirtualKeys.VK_ENTER;
+            NativeFunctions.SendInput(1, inputs, Marshal.SizeOf(inputs[0]));
+            //Thread.Sleep(2000);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
